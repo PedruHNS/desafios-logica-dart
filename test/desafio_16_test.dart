888f827656f46624d16a1fd55477e19600ca76bb
Pedro Siqueira <pedro.siqueira1999@hotmail.com>
2024-07-17 00:08:06 -0300

    desafio 16

--- test/desafio_16_test.dart
+++ test/desafio_16_test.dart
@@ -2,18 +2,29 @@ import 'package:desafio_logica/desafio_16.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('desconto inss', () {
-    final name = 'João';
-    final grossSalary = 1000.00;
+  group(
+    'teste classe Funcionario | ',
+    () {
+      // arrange  | config
+      final name = 'João';
+      final grossSalary = 1000.00;
+      final employee = Employee(name: name, grossSalary: grossSalary);
 
-    final employee = Employee(
-      name: name,
-      grossSalary: grossSalary,
-    );
+      test('desonco inss 8.5%', () {
+        // act | ação
+        final descont = employee.descont;
 
-    expect(employee.name, name);
-    expect(employee.grossSalary, grossSalary);
-    expect(employee.descont, 85.00);
-    expect(employee.netSalary, 915.00);
-  });
+        // assert | comparação
+        expect(descont, 85.00);
+      });
+
+      test('salário líquido', () {
+        // act | ação
+        final netSalary = employee.netSalary;
+
+        // assert | comparação
+        expect(netSalary, 915.00);
+      });
+    },
+  );
 }
